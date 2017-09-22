47bfa7cda094f06917c7612c4d46e3b64386fb8d
John Hartert <j.blainehartert@gmail.com>
2017-09-22 10:03:34 -0400

    Adds spreadsheet.py and adds steps in comments at the top

---
+++ spreadsheet.py
@@ -0,0 +1,3 @@
+# Copy of spreadsheet here: https://docs.google.com/spreadsheets/d/1SnJhFzEFpMPCwYgSKQu3gVfDc3JCtBh8LRAQOiUibKo/edit#gid=152787366
+# Shared this email with google sheets: legislators@python-sheets-180602.iam.gserviceaccount.com
+# Installed: pip install gspread oauth2client
