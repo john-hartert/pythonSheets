278e675ad22e353476adc1a8cc6f23dc001f9753
John Hartert <j.blainehartert@gmail.com>
2017-10-06 15:45:47 -0400

    Adds comments

--- spreadsheet.py
+++ spreadsheet.py
@@ -8,6 +8,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # This uses credentials for API.
+# Got h#####. I should probably change this in a .env.
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
